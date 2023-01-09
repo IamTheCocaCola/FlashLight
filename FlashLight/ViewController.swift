243d0c6e07304ee5b3f1f6ee3e0243b3a8ee9480
Sawyer Mallonee <temple77@comcast.net>
2023-01-09 09:57:26 -0700

    Initial Commit

---
+++ FlashLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashLight
+//
+//  Created by Mallonee, Sawyer - Student on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
