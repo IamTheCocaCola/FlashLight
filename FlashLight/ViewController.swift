64d7c47c0bc23fdc2a027550bcb2a1d79186dbe9
Sawyer Mallonee <temple77@comcast.net>
2023-02-23 09:37:07 -0700

    Final

--- FlashLight/ViewController.swift
+++ FlashLight/ViewController.swift
@@ -14,9 +14,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Button(_ sender: Any) {
-        self.view.backgroundColor = UIColor.blue
+    @IBAction func Colors(_ sender: UIButton) {
+        sender.tag += 1
+        if sender.tag > 3 {sender.tag = 0}
+        
+        switch sender.tag {
+        case 1:
+            self.view.backgroundColor = UIColor.red
+        case 2:
+            self.view.backgroundColor =
+            UIColor.blue
+        case 3:
+            self.view.backgroundColor =
+            UIColor.green
+        default:
+            self.view.backgroundColor =
+            UIColor.red
+        }
     }
     
+    @IBAction func Reset(_ sender: Any) {
+        self.view.backgroundColor =
+        UIColor.white
+    }
 }
-
